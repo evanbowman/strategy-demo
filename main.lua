d2e66eb967d3b2f38e48923572ce3dd2a2a36ea6
evanbowman <ebowman@bu.edu>
2020-11-07 16:44:35 -0500

    initial commit, copy over demo from engine repo

---
+++ main.lua
@@ -0,0 +1,87 @@
+
+fade(1)
+
+txtr(0, "overlay.bmp")
+
+tile(0, 9, 9, 28)
+tile(0, 8, 9, 27)
+tile(0, 7, 9, 16)
+tile(0, 6, 9, tile(0, 9, 9))
+
+print("Hello, world!", 3, 3)
+
+txtr(2, "tile0.bmp")
+
+
+for i = 15, 45 do
+   for j = 15, 45 do
+      tile(2, i, j, 1)
+   end
+end
+
+
+fade(0)
+
+
+txtr(4, "spritesheet.bmp")
+
+
+local cursor = { x=31, y=31 }
+
+
+function hex_tile(x, y, t)
+   -- TODO
+end
+
+
+function main_loop(update, draw)
+   while true do
+      update(delta())
+      clear()
+      draw()
+      display()
+   end
+end
+
+
+local x = 0
+local y = 0
+
+local i = 0
+
+
+function update(dt)
+
+   if btnp(0) then
+      fade(0.5)
+      i = (i + 1) % 250
+   elseif btnnp(0) then
+      fade(0)
+   end
+
+   if btn(5) then
+      x = x + 2
+   elseif btn(4) then
+      x = x - 2
+   end
+
+   if btn(6) then
+      y = y - 2
+   elseif btn(7) then
+      y = y + 2
+   end
+
+   camera(x, y)
+end
+
+
+function draw()
+   spr(0, x, y)
+   spr(255, 30, 30)
+end
+
+
+print(tostring(collectgarbage("count") * 1024), 3, 5)
+
+
+main_loop(update, draw)
