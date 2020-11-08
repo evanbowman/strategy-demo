3c508434ce784c00f7e475a106e5399bad462b3b
evanbowman <ebowman@bu.edu>
2020-11-07 22:17:14 -0500

    create simple hex grid

--- main.lua
+++ main.lua
@@ -1,37 +1,68 @@
 
+function hex_tile(x, y, t)
+   -- Interleave columns btwn layers; eats up two tile layers, but saves vram on
+   -- the tile corners.
+   local l = 1
+   local x_off = 0
+   local y_off = 1
+
+   if x % 2 == 0 then
+      l = 2
+      x_off = 0
+      y_off = 0
+   end
+
+   -- hex tiles start at index 1, 8 hardware tiles in size
+   local cur = 1 + (t * 8)
+
+   for j = y, y + 1 do
+      for i = x, x + 3 do
+         tile(l, x_off + i + x * 2, y_off + j + y, cur)
+         cur = cur + 1
+      end
+   end
+end
+
+function which_tile(x, y)
+
+end
+
+cursor = { x=0, y=0 }
+
+function cursor_pos()
+   if cursor.x % 2 == 0 then
+      return cursor.x * 8 * 3, cursor.y * 8 * 2
+   else
+      return cursor.x * 8 * 3, cursor.y * 8 * 2 + 8
+   end
+end
+
 fade(1)
 
 txtr(0, "overlay.bmp")
 
-tile(0, 9, 9, 28)
-tile(0, 8, 9, 27)
-tile(0, 7, 9, 16)
-tile(0, 6, 9, tile(0, 9, 9))
-
 print("Hello, world!", 3, 3)
 
+txtr(1, "tile0.bmp")
 txtr(2, "tile0.bmp")
 
-
-for i = 15, 45 do
-   for j = 15, 45 do
-      tile(2, i, j, 1)
+for x = 0, 20 do
+   for y = 0, 30 do
+      hex_tile(x, y, 0)
    end
 end
+
+hex_tile(3, 3, 1)
+hex_tile(3, 4, 1)
+hex_tile(4, 4, 1)
 
 
 fade(0)
 
-
 txtr(4, "spritesheet.bmp")
 
 
-local cursor = { x=31, y=31 }
-
-
-function hex_tile(x, y, t)
-   -- TODO
-end
+local cam = { x=0, y=0 }
 
 
 function main_loop(update, draw)
@@ -43,45 +74,39 @@ function main_loop(update, draw)
    end
 end
 
-
-local x = 0
-local y = 0
-
-local i = 0
-
-
 function update(dt)
 
-   if btnp(0) then
-      fade(0.5)
-      i = (i + 1) % 250
-   elseif btnnp(0) then
-      fade(0)
+   if btnp(5) then
+      cursor.x = cursor.x + 1
+   elseif btnp(4) then
+      cursor.x = cursor.x - 1
    end
 
-   if btn(5) then
-      x = x + 2
-   elseif btn(4) then
-      x = x - 2
+   if btnp(6) then
+      cursor.y = cursor.y - 1
+   elseif btnp(7) then
+      cursor.y = cursor.y + 1
    end
 
-   if btn(6) then
-      y = y - 2
-   elseif btn(7) then
-      y = y + 2
-   end
-
-   camera(x, y)
+   local cx, cy = cursor_pos()
+   cam.x = lerp(cam.x, cx + 15.9, dt * 0.000002)
+   cam.y = lerp(cam.y, cy + 0.1, dt * 0.000002)
 end
 
+function lerp(v0, v1, t)
+   return (1 - t) * v0 + t * v1;
+end
 
 function draw()
-   spr(0, x, y)
-   spr(255, 30, 30)
+   local cx, cy = cursor_pos()
+   spr(1, cx, cy)
+   spr(2, cx + 16, cy)
+   spr(3, cx, cy + 16)
+   spr(4, cx + 16, cy + 16)
+
+   camera(cam.x, cam.y)
 end
 
-
 print(tostring(collectgarbage("count") * 1024), 3, 5)
-
 
 main_loop(update, draw)
